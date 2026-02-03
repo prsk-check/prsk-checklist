1b332a60cb55de8d6ab9242d9e31741ccfdd24fd
prsk-check <pani0607@gmail.com>
2026-02-03 20:54:32 +0900

    Update script.js
    한정헤어 탭 추가

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ document.querySelectorAll('.tab-button').forEach(button => {
   });
 });
 
-
 const cat01Images = ['0101', '0102', '0103', '0104', '0105', '0106', '0107', '0108', '0109', '0110', '0111', '0112', '0113', '0114', '0115', '0116', '0117', '0118', '0119', '0120', '0121', '0122', '0123', '0124', '0125', '0126', '0127'];
 const cat02Images = ['0201', '0202', '0203', '0204', '0205', '0206', '0207', '0208', '0209', '0210', '0211', '0212', '0213', '0214', '0215', '0216', '0217', '0218', '0219', '0220', '0221', '0222', '0223', '0224', '0225'];
 const cat03Images = ['0301', '0302', '0303', '0304', '0305', '0306', '0307', '0308', '0309', '0310', '0311', '0312', '0313', '0314', '0315', '0316', '0317', '0318', '0319', '0320', '0321', '0322', '0323', '0324', '0325', '0326'];
@@ -72,14 +71,42 @@ const normal_cat52Images = ['5201', '5202', '5204', '5205', '5207', '5208', '520
 const normal_cat53Images = ['5301', '5302', '5303', '5304', '5305', '5307', '5309', '5310', '5312', '5314', '5318', '5319', '5321', '5324', '5326'];
 const normal_cat54Images = ['5401', '5402', '5403', '5404', '5406', '5408', '5409', '5410', '5412', '5415', '5418', '5419', '5421', '5423'];
 
-function createCategoryImages(categoryId, imageNames) {
+const hair_cat01Images = ['00101', '00102', '00103', '00104', '00105', '00106', '00107', '00108', '00109', '00110', '00111', '00112'];
+const hair_cat02Images = ['00201', '00202', '00203', '00204', '00205', '00206', '00207', '00208'];
+const hair_cat03Images = ['00301', '00302', '00303', '00304', '00305', '00306', '00307'];
+const hair_cat04Images = ['00401', '00402', '00403', '00404', '00405', '00406', '00407', '00408'];
+const hair_cat05Images = ['00501', '00502', '00503', '00504', '00505', '00506', '00507'];
+const hair_cat06Images = ['00601', '00602', '00603', '00604', '00605', '00606', '00607', '00608'];
+const hair_cat11Images = ['001101', '001102', '001103', '001104', '001105', '001106', '001107', '001108', '001109'];
+const hair_cat12Images = ['001201', '001202', '001203', '001204', '001205', '001206', '001207', '001208', '001209'];
+const hair_cat13Images = ['001301', '001302', '001303', '001304', '001305', '001306', '001307', '001308', '001309'];
+const hair_cat14Images = ['001401', '001402', '001403', '001404', '001405', '001406', '001407', '001408', '001409'];
+const hair_cat21Images = ['002101', '002102', '002103', '002104', '002105', '002106', '002107', '002108', '002109'];
+const hair_cat22Images = ['002201', '002202', '002203', '002204', '002205', '002206', '002207', '002208', '002209'];
+const hair_cat23Images = ['002301', '002302', '002303', '002304', '002305', '002306', '002307', '002308'];
+const hair_cat24Images = ['002401', '002402', '002403', '002404', '002405', '002406', '002407', '002408'];
+const hair_cat31Images = ['003101', '003102', '003103', '003104', '003105', '003106', '003107', '003108'];
+const hair_cat32Images = ['003201', '003202', '003203', '003204', '003205', '003206', '003207', '003208', '003209'];
+const hair_cat33Images = ['003301', '003302', '003303', '003304', '003305', '003306', '003307', '003308', '003309', '003310'];
+const hair_cat34Images = ['003401', '003402', '003403', '003404', '003405', '003406', '003407', '003408', '003409'];
+const hair_cat41Images = ['004101', '004102', '004103', '004104', '004105', '004106', '004107', '004108', '004109'];
+const hair_cat42Images = ['004201', '004202', '004203', '004204', '004205', '004206', '004207', '004208', '004209'];
+const hair_cat43Images = ['004301', '004302', '004303', '004304', '004305', '004306', '004307', '004308'];
+const hair_cat44Images = ['004401', '004402', '004403', '004404', '004405', '004406', '004407', '004408'];
+const hair_cat51Images = ['005101', '005102', '005103', '005104', '005105', '005106', '005107', '005108', '005109'];
+const hair_cat52Images = ['005201', '005202', '005203', '005204', '005205', '005206', '005207', '005208'];
+const hair_cat53Images = ['005301', '005302', '005303', '005304', '005305', '005306', '005307', '005308'];
+const hair_cat54Images = ['005401', '005402', '005403', '005404', '005405', '005406', '005407', '005408', '005409'];
+
+
+function createCategoryImages(categoryId, imageNames, folder = 'images') { 
   const container = document.getElementById(categoryId);
   imageNames.forEach(name => {
-    const onSrc = `images/${name}.png`;
-    const offSrc = `images/${name}_.png`;
+    const onSrc = `${folder}/${name}.png`;
+    const offSrc = `${folder}/${name}_.png`;
 
     const img = document.createElement('img');
-    img.src = offSrc;
+    img.src = offSrc; 
     img.dataset.on = onSrc;
     img.dataset.off = offSrc;
 
@@ -146,6 +173,33 @@ createCategoryImages('normal_cat52', normal_cat52Images);
 createCategoryImages('normal_cat53', normal_cat53Images);
 createCategoryImages('normal_cat54', normal_cat54Images);
 
+createCategoryImages('hair_cat01', hair_cat01Images, 'hair');
+createCategoryImages('hair_cat02', hair_cat02Images, 'hair');
+createCategoryImages('hair_cat03', hair_cat03Images, 'hair');
+createCategoryImages('hair_cat04', hair_cat04Images, 'hair');
+createCategoryImages('hair_cat05', hair_cat05Images, 'hair');
+createCategoryImages('hair_cat06', hair_cat06Images, 'hair');
+createCategoryImages('hair_cat11', hair_cat11Images, 'hair');
+createCategoryImages('hair_cat12', hair_cat12Images, 'hair');
+createCategoryImages('hair_cat13', hair_cat13Images, 'hair');
+createCategoryImages('hair_cat14', hair_cat14Images, 'hair');
+createCategoryImages('hair_cat21', hair_cat21Images, 'hair');
+createCategoryImages('hair_cat22', hair_cat22Images, 'hair');
+createCategoryImages('hair_cat23', hair_cat23Images, 'hair');
+createCategoryImages('hair_cat24', hair_cat24Images, 'hair');
+createCategoryImages('hair_cat31', hair_cat31Images, 'hair');
+createCategoryImages('hair_cat32', hair_cat32Images, 'hair');
+createCategoryImages('hair_cat33', hair_cat33Images, 'hair');
+createCategoryImages('hair_cat34', hair_cat34Images, 'hair');
+createCategoryImages('hair_cat41', hair_cat41Images, 'hair');
+createCategoryImages('hair_cat42', hair_cat42Images, 'hair');
+createCategoryImages('hair_cat43', hair_cat43Images, 'hair');
+createCategoryImages('hair_cat44', hair_cat44Images, 'hair');
+createCategoryImages('hair_cat51', hair_cat51Images, 'hair');
+createCategoryImages('hair_cat52', hair_cat52Images, 'hair');
+createCategoryImages('hair_cat53', hair_cat53Images, 'hair');
+createCategoryImages('hair_cat54', hair_cat54Images, 'hair');
+
 
 document.getElementById('selectAllButton').addEventListener('click', () => {
   const allImages = document.querySelectorAll('.tab-content.active .image-grid img');
@@ -161,7 +215,6 @@ document.getElementById('resetButton').addEventListener('click', () => {
   });
 });
 
-
 document.getElementById("captureButton").addEventListener("click", function () {
   html2canvas(document.body).then((canvas) => {
     const link = document.createElement("a");
@@ -170,7 +223,6 @@ document.getElementById("captureButton").addEventListener("click", function () {
     link.click();
   });
 });
-
 
 
 
